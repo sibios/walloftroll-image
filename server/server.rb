09efe220af3566305c7468472ea8b145f455b789
sibios <sibios@gmail.com>
2014-08-27 22:38:36 -0700

    woo! working server!

--- server/server.rb
+++ server/server.rb
@@ -13,17 +13,56 @@
 
 require 'sinatra/base'
 require 'digest'
+require 'find'
+require 'json'
 
 class WoSTHTTPServer < Sinatra::Base
-	#this needs to be defined for all images to serve
-	get '/goatse' do
-		#need to add a temporary route for Goatse image (to avoid caching on WoS)
-		token = Digest::SHA256.hexdigest("goatse"+Time.now.to_s)
-		puts "[DEBUG] Adding route to goatse.jpg as \"/#{token}\""
-		WoSTHTTPServer.get "/#{token}" do
-			send_file('img/nsfw/goatse.jpg', :filename => token)
+	images = {}
+	#build file listing
+	Find.find(Dir.pwd+'/img') do |path|
+		if FileTest.directory?(path)
+			if File.basename(path)[0] == ?.
+				Find.prune
+			else
+				next
+			end
+		else
+			key = File.basename(path,".*")
+			unless images[key].nil?
+				puts "[WARNING] Had a name collision on #{key}!\n\tFile 1: #{images[key][:path]}\n\tFile 2: #{path}"
+				next
+			end
+
+			ext_detected = File.extname(path)
+
+			extension = ext_detected.match(/^\.(jpg|jpeg|png|gif)$/i)
+			next if extension.nil?
+			extension = extension[0]
+			extension.downcase!
+			extension.gsub!(".","")
+
+			sfw = path.match(/\/nsfw\//i).nil?
+			puts "[DEBUG] Found SFW?(#{sfw}) image named \"#{key}\" of type #{extension} at #{path}"
+
+			images[key] = {:extension => extension, :path => path, :sfw? => sfw}
 		end
-		redirect "/#{token}"
+	end
+	#this needs to be defined for all images to serve
+	images.keys.each do |key|
+		puts "[DEBUG] Adding static route for /#{key}"
+		get "/#{key}" do
+			#need to add a temporary route for Goatse image (to avoid caching on WoS)
+			token = Digest::SHA256.hexdigest(key+Time.now.to_s)
+			puts "[DEBUG] Adding dynamic route to #{key} as \"/#{token}\""
+			WoSTHTTPServer.get "/#{token}" do
+				send_file(images[key][:path], :filename => token, :disposition => :inline)
+			end
+			redirect "/#{token}"
+		end
+	end
+
+	get "/LIST" do
+		images.to_json
 	end
 
 	run! if app_file == $0
